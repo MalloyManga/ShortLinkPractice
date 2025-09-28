e3d69dfde7de6378de93971a3d62f17afa0951cf
Malloy <li3210361050@gmail.com>
2025-09-28 20:39:16 +0800

    feat: add a input component from my custom component library

---
+++ frontend/app/components/Input.vue
@@ -0,0 +1,49 @@
+<!-- comp/Input.vue -->
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import ScaleProgressBar from './ScaleProgressBar.vue'
+
+interface Props {
+    iptName: string
+    iptLabel: string
+}
+const { iptName = 'ipt6', iptLabel = 'Name' } = defineProps<Props>()
+const iptValue = defineModel<string>('iptValue')
+const isFocused = ref(false)
+
+const handleFocused = () => {
+    isFocused.value = true
+}
+const handleBlur = () => {
+    isFocused.value = false
+}
+
+const iptPrgBarSCale = computed(() => {
+    return isFocused.value ? 'scale-x-100' : 'scale-x-0'
+})
+const chars = computed(() => {
+    return [...iptLabel]
+})
+const isActive = computed(() => {
+    return isFocused.value || (iptValue.value && iptValue.value.trim() != '')
+})
+
+</script>
+
+<template>
+        <div>
+        <div class="relative inline-block">
+            <label for="ipt" class="absolute left-2 text-gray-400 cursor-text">
+                <span v-for="(char, index) in chars" :key="index"
+                    :class="isActive ? 'text-[#ADFF2F] -translate-y-[21px] scale-90' : ''"
+                    class="inline-block transition-all duration-300 ease-[cubic-bezier(0.68,-0.55,0.265,1.55)]"
+                    :style="`transition-delay: ${(index + 1) * 50}ms`">
+                    {{ char }}
+                </span>
+            </label>
+            <input type="text" id="ipt" :name="iptName" @focus="handleFocused" @blur="handleBlur" v-model="iptValue"
+                class="outline-none bg-transparent w-44 pl-2 border-b-[1px] border-gray-400">
+            <ScaleProgressBar prg-bgi="background-color: #ADFF2F;" :prg-scale="iptPrgBarSCale" prg-width="w-44" />
+        </div>
+    </div>
+</template>
---
+++ frontend/app/components/ScaleProgressBar.vue
@@ -0,0 +1,23 @@
+<!-- ScaleProgressBar.vue -->
+<script setup lang="ts">
+interface Prop {
+    prgBgi?: string
+    prgWidth?: string
+    prgBgc?: string
+    prgDura?: string
+    prgScale: string
+    prgOri?: string
+    prgHt?: string
+    prgPos?: string
+}
+
+const { prgBgi, prgWidth, prgBgc = 'bg-sky-300', prgDura = 'duration-300', prgScale, prgHt = 'h-1', prgOri, prgPos = 'left-0 bottom-0' } = defineProps<Prop>()
+
+</script>
+
+<template>
+    <span class="absolute transition-all" :class="[prgDura, prgWidth, prgScale, prgOri, prgBgc, prgHt, prgPos]"
+        :style="prgBgi"></span>
+
+
+</template>
