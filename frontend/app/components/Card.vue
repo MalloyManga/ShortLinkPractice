3aa4c38a2d0a760105a2f4d1b9fea10365e426bb
Malloy <li3210361050@gmail.com>
2025-09-28 21:31:28 +0800

    feat: add card component

---
+++ frontend/app/components/Card.vue
@@ -0,0 +1,11 @@
+<!-- comp/Card.vue -->
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="border-2 border-[#ADFF2F] p-12 rounded-lg grid grid-rows-3 gap-12 items-center justify-items-center">
+        <slot name="ipt"></slot>
+        <slot name="btn"></slot>
+    </div>
+</template>
