7058df39311514c7f99cf81d585b8a3026dbad46
Malloy <li3210361050@gmail.com>
2025-09-28 21:31:48 +0800

    feat: all styles over😎

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,9 +1,18 @@
 <!-- pages/index.vue -->
 <script setup lang="ts">
 
-
 </script>
 
 <template>
-
+    <div>
+        <Card>
+            <template #ipt>
+                <Input ipt-label="Origin Link" ipt-name="origin_link" ipt-width="w-96" />
+                <Input ipt-label="Short Link" ipt-name="short_link" ipt-width="w-96" />
+            </template>
+            <template #btn>
+                <Button btn-content="Get shortlink!" />
+            </template>
+        </Card>
+    </div>
 </template>
