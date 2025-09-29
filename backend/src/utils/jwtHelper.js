f2281d80de8bcb0a8f82185f8666dea2b7ad2f07
Malloy <li3210361050@gmail.com>
2025-09-29 17:22:14 +0800

    feat: add jwtHelper file to initialize token authorization structure

--- backend/src/utils/jwtHelper.js
+++ backend/src/utils/jwtHelper.js
@@ -1,1 +1,24 @@
 // jwtHelper.js
+import * as jose from 'jose'
+import { AppError } from './AppError.js'
+
+const JWT_SECRET_KTY = process.env.JWT_SECRET_KTY
+const secret = new TextEncoder().encode(JWT_SECRET_KTY)
+const alg = 'HS256'
+
+export async function generateToken(data) {
+    const token = await new jose.SignJWT(data)
+        .setProtectedHeader({ alg })
+        .setExpirationTime('2m')
+        .sign(secret)
+    return token
+}
+
+export async function verifyToken(token) {
+    try {
+        const result = await jose.jwtVerify(token, secret)
+        return result
+    } catch (error) {
+        throw new AppError('NoToken or token is invalid!', 401, 'UnauthorizedError')
+    }
+}
