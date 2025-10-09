5b7adeedeaf4aca23a1151e592b0765050cbcdec
Malloy <li3210361050@gmail.com>
2025-10-09 16:35:58 +0800

    feat: add a route to signin in URL '/signin'

--- backend/src/routes/users.route.js
+++ backend/src/routes/users.route.js
@@ -2,12 +2,15 @@
 import { Router } from "express"
 import {
     createUser,
+    userVerifyWhenLogin
 } from "../controllers/users.controller.js"
 
 const router = Router()
 
 router.route('/signup')
     .post(createUser)
-
+    
+router.route('/signin')
+    .post(userVerifyWhenLogin)
 
 export default router
