c5d6e3dc469e8862c9edf88fd35d1b7c63c9370c
Malloy <li3210361050@gmail.com>
2025-10-10 20:19:35 +0800

    feat: add users sign up logic in frontend

---
+++ frontend/app/composables/useUsers.ts
@@ -0,0 +1,26 @@
+// useUsers.ts
+interface SignUpData {
+    name: string
+    email: string
+    password: string
+}
+
+export const useSignUp = () => {
+    const signUpDataForBody = ref<SignUpData | null>(null)
+    const { data, status, error, execute } = useFetch('https://localhost:3001/users/signup', {
+        method: 'POST',
+        immediate: false,
+        watch: false,
+        body: signUpDataForBody
+    })
+    const signUp = async (signUpData: SignUpData) => {
+        signUpDataForBody.value = signUpData
+        await execute()
+    }
+    return {
+        signUp,
+        data,
+        status,
+        error
+    }
+}
