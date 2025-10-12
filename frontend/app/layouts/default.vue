e03ea0ded309173e631f9fa44b71d8943aaaebd0
Malloy <li3210361050@gmail.com>
2025-10-12 19:04:04 +0800

    style: change background color darker

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -9,7 +9,7 @@
         <Nav />
     </header>
 
-    <main class="bg-[#f5f5f5] w-screen h-screen inline-flex justify-center items-center">
+    <main class="bg-[#333] w-screen h-screen inline-flex justify-center items-center">
         <NuxtPage />
     </main>
 </template>
