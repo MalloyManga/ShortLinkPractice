025d7344da66e7990c64e9b6198a841bebd994d8
Malloy <li3210361050@gmail.com>
2025-10-13 17:48:38 +0800

    feat: add a new slot to show error and status message

--- frontend/app/components/Card.vue
+++ frontend/app/components/Card.vue
@@ -7,5 +7,6 @@
     <div class="border-2 border-[#ADFF2F] p-12 rounded-lg grid grid-rows-3 gap-12 items-center justify-items-center">
         <slot name="ipt"></slot>
         <slot name="btn"></slot>
+        <slot name="footer"></slot>
     </div>
 </template>
