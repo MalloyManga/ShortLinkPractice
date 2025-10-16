e5c4c4891623bdf02d8f6396de41dea0c5a2e3b0
Malloy <li3210361050@gmail.com>
2025-10-16 15:42:45 +0800

    style: add a style on card appear and disappear

--- frontend/app/components/Nav.vue
+++ frontend/app/components/Nav.vue
@@ -26,14 +26,50 @@ function modalClose() {
         </button>
     </div>
 
-    <div v-if="isModalOpen"
-        class="fixed top-0 left-0 z-1 w-screen h-screen bg-black/70 flex justify-center items-center"
-        @click="modalClose">
-        <div v-if="!isLoggedIn" class="relative z-2" @click.stop>
-            <Form @logined="modalClose" />
+    <Transition name="fade">
+        <div v-if="isModalOpen"
+            class="fixed top-0 left-0 z-1 w-screen h-screen bg-black/70 flex justify-center items-center"
+            @click="modalClose">
+            <Transition name="scale-card" appear>
+                <div class="relative z-2" @click.stop>
+                    <Form @logined="modalClose" v-if="!isLoggedIn" />
+                    <UserInforCard v-else />
+                </div>
+            </Transition>
         </div>
-        <div v-else class="relative z-2" @click.stop>
-            <UserInforCard />
-        </div>
-    </div>
+    </Transition>
 </template>
+
+<style scoped>
+/* modal fade in and out */
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 300ms;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+    opacity: 1;
+}
+
+/* card scale enter and leave */
+.scale-card-enter-active,
+.scale-card-leave-active {
+    transition: transform 300ms;
+}
+
+.scale-card-enter-from,
+.scale-card-leave-to {
+    transform: scale(0);
+}
+
+.scale-card-enter-to,
+.scale-card-leave-from {
+    transform: scale(1);
+}
+</style>
